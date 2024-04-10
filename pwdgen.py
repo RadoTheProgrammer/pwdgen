6e970d0b1007911c0da7bf4d10913a28cf3f88fc
RadoTheProgrammer <rado@arazakar.com>
2024-04-10 11:32:44 +0200

    + main()

--- pwdgen.py
+++ pwdgen.py
@@ -22,3 +22,12 @@ def generate(syllables=6):
     else:
         pw=number+pw
     return pw
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="A simple password generator")
+    parser.add_argument("-s","syllables", type=int, default=6, help="Number of syllables in the password")
+    parser.add_argument("-n","number", type=int, default=1, help="Number of passwords to generate")
+    args = parser.parse_args()
+    for _ in range(args.number):
+        print(generate(args.syllables))
